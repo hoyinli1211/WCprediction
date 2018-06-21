7ca19e531ed36788cb4a88f66683a8cdb93264d9
hoyinli1211 <hoyinli.r@gmail.com>
2018-06-21 23:48:42 +0800

    Update Script.R

--- Script.R
+++ Script.R
@@ -38,7 +38,7 @@ url3 <- "https://raw.githubusercontent.com/hoyinli1211/WCprediction/master/histo
 df.result <- read.csv(url3)
 #View(df.result)
 df.result2 <- df.result %>%
-                mutate(date=as.Date(date)) %>%
+                mutate(date=as.Date(as.character(date))) %>%
                 filter(home_team %in% v.cty & away_team %in% v.cty) %>% # filter those attend 2018 world cup only
                 filter((home_team == 'Russia' & neutral == FALSE) | (home_team != 'Russia' & away_team != 'Russia' & neutral == TRUE)) %>% # consider home effect
                 filter(date >= as.Date('2010-01-01')) #filter those long time ago
