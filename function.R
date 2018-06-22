5fd8c129a31bb9a6a78c5dd005d836168f092b9d
hoyinli1211 <hoyinli.r@gmail.com>
2018-06-22 15:44:19 +0800

    Update function.R

--- function.R
+++ function.R
@@ -18,5 +18,29 @@ defence_rating <- function(cty) {
   return(sum(df$prodsum)/sum(df$weight))
 }
 
+attack_rating2 <- function(cty) {
+  
+  df <- df.result2 %>% 
+          filter(home_team==cty | away_team==cty) %>%
+          mutate(attack=ifelse(home_team==cty, home_score,away_score),
+                 finalweight=weight*weight1,
+                 prodsum=attack*finalweight)
+  return(sum(df$prodsum)/sum(df$finalweight))
+}
+
+defence_rating2 <- function(cty) {
+  
+  df <- df.result2 %>% 
+    filter(home_team==cty | away_team==cty) %>%
+    mutate(defence=ifelse(home_team==cty, away_score, home_score),
+                 finalweight=weight*weight1,
+                 prodsum=attack*finalweight)
+  return(sum(df$prodsum)/sum(df$finalweight))
+}
+
+
+
 sapply(v.cty,function(x) attack_rating(x))
 sapply(v.cty,function(x) defence_rating(x))
+sapply(v.cty,function(x) attack_rating2(x))
+sapply(v.cty,function(x) defence_rating2(x))
