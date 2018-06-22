bed0572250fbf40df11ad66d51e29a2b3bcb1dad
hoyinli1211 <hoyinli.r@gmail.com>
2018-06-22 23:40:08 +0800

    Update Script.R

--- Script.R
+++ Script.R
@@ -12,7 +12,7 @@
 ################################
 #Package management
 ################################
-list.of.packages <- c("tidyverse","stringr", "rvest","dplyr","ggplot2","cowplot","tabulizer","lubridate")
+list.of.packages <- c("tidyverse","stringr", "rvest","dplyr","ggplot2","cowplot","lubridate")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only = TRUE)
