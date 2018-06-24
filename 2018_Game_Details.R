f8905cf811ad1ef2753e79573ec75badb24994a9
hoyinli1211 <hoyinli.r@gmail.com>
2018-06-24 16:42:28 +0800

    Create 2018_Game_Details.R

---
+++ 2018_Game_Details.R
@@ -0,0 +1,56 @@
+library(rvest)
+
+#######################
+#url 
+#######################
+
+  #teams statistics
+url.team.topgoals <- "https://www.fifa.com/worldcup/statistics/teams/goal-scored"
+url.team.attempts <- "https://www.fifa.com/worldcup/statistics/teams/shots"
+url.team.disciplinary <- "https://www.fifa.com/worldcup/statistics/teams/disciplinary"
+  #players statistics
+url.player.scored <- "https://www.fifa.com/worldcup/statistics/players/goal-scored"
+url.player.saves <- "https://www.fifa.com/worldcup/statistics/players/saves"
+url.player.shots <- "https://www.fifa.com/worldcup/statistics/players/shots"
+url.player.disciplinary <- "https://www.fifa.com/worldcup/statistics/players/disciplinary"
+
+#######################
+#data scrape
+#######################
+
+  #team
+df.team.topgoals <- read_html(url.team.topgoals) %>%
+                html_nodes("table") %>%
+                .[1] %>%
+                html_table(fill=TRUE)
+
+df.team.attempts <- read_html(url.team.attempts) %>%
+  html_nodes("table") %>%
+  .[1] %>%
+  html_table(fill=TRUE)
+
+df.team.disciplinary <- read_html(url.team.disciplinary) %>%
+  html_nodes("table") %>%
+  .[1] %>%
+  html_table(fill=TRUE)
+
+  #player
+df.player.scored <- read_html(url.player.scored) %>%
+  html_nodes("table") %>%
+  .[1] %>%
+  html_table(fill=TRUE)
+
+df.player.saves <- read_html(url.player.saves) %>%
+  html_nodes("table") %>%
+  .[1] %>%
+  html_table(fill=TRUE)
+
+df.player.shots <- read_html(url.player.shots) %>%
+  html_nodes("table") %>%
+  .[1] %>%
+  html_table(fill=TRUE)
+
+df.player.disciplinary <- read_html(url.player.disciplinary) %>%
+  html_nodes("table") %>%
+  .[1] %>%
+  html_table(fill=TRUE)
