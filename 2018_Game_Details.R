251dbef02ec941be472d016e8a1d69b73adb9a3d
hoyinli1211 <hoyinli.r@gmail.com>
2018-06-24 21:55:39 +0800

    Update 2018_Game_Details.R

--- 2018_Game_Details.R
+++ 2018_Game_Details.R
@@ -34,6 +34,7 @@ url.team.attempts <- "https://www.fifa.com/worldcup/statistics/teams/shots"
 url.team.disciplinary <- "https://www.fifa.com/worldcup/statistics/teams/disciplinary"
   #players statistics
 url.player.main <- "https://raw.githubusercontent.com/hoyinli1211/WCprediction/master/Data/PlayerList.csv"
+url.player.main2 <- "https://raw.githubusercontent.com/hoyinli1211/WCprediction/master/Player.csv"
 url.player.profile <- "https://www.fifa.com/worldcup/players/browser/#player-by-position"
 url.player.scored <- "https://www.fifa.com/worldcup/statistics/players/goal-scored"
 url.player.saves <- "https://www.fifa.com/worldcup/statistics/players/saves"
@@ -65,8 +66,12 @@ df.team.disciplinary <- read_html(url.team.disciplinary) %>%
 
   #player
 
-df.player.main <- read.csv(url.player.main) %>%
-                    mutate_if(sapply(df.player.main, is.factor), as.character)
+df.player.main <- read.csv(url.player.main) 
+df.player.main <- df.player.main %>%
+                    mutate_if(is.factor, as.character)
+
+df.player.main2 <- read.csv(url.player.main2) %>%
+                    mutate_if(is.factor, as.character)
 
 # df.player.profile <- read_html(url.player.profile) %>%
 #                       html_nodes("table") %>%
@@ -114,4 +119,5 @@ df.player.MAIN <- df.player.main %>% left_join(df.player.FIFA, by=c('FIFA.Popula
 dim(df.player.MAIN %>% filter(!is.na(Player)))
 unique(c(df.player.scored$Player,df.player.saves$Player, df.player.shots$Player, df.player.disciplinary$Player))
 
-
+df.player.MAIN[!df.player.MAIN$FIFA.Popular.Name %in% df.player.FIFA$Player]$FIFA.Popular.Name
+df.player.FIFA %>% filter(!df.player.FIFA$Player2 %in% df.player.MAIN$FIFA.Popular.Name) %>% select(Player2)
