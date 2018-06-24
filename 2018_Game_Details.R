1d9d178b74bd513d4f745ecd9d6c6f532ff7b5b2
hoyinli1211 <hoyinli.r@gmail.com>
2018-06-24 16:58:47 +0800

    Update 2018_Game_Details.R

--- 2018_Game_Details.R
+++ 2018_Game_Details.R
@@ -9,6 +9,7 @@ url.team.topgoals <- "https://www.fifa.com/worldcup/statistics/teams/goal-scored
 url.team.attempts <- "https://www.fifa.com/worldcup/statistics/teams/shots"
 url.team.disciplinary <- "https://www.fifa.com/worldcup/statistics/teams/disciplinary"
   #players statistics
+url.player.main <- "https://raw.githubusercontent.com/hoyinli1211/WCprediction/master/Data/PlayerList.csv"
 url.player.scored <- "https://www.fifa.com/worldcup/statistics/players/goal-scored"
 url.player.saves <- "https://www.fifa.com/worldcup/statistics/players/saves"
 url.player.shots <- "https://www.fifa.com/worldcup/statistics/players/shots"
@@ -35,10 +36,15 @@ df.team.disciplinary <- read_html(url.team.disciplinary) %>%
   html_table(fill=TRUE)
 
   #player
+
+df.player.main <- read.csv(url.player.main)
+
 df.player.scored <- read_html(url.player.scored) %>%
   html_nodes("table") %>%
   .[1] %>%
-  html_table(fill=TRUE)
+  html_table(fill=TRUE) %>%
+  .[[1]]
+colnames(df.player.scored) <- c('rank.scored','player','goal.scored','goal.assist','mins.player','match.play','penalties.score','left.scored','right.scored','head.scored')
 
 df.player.saves <- read_html(url.player.saves) %>%
   html_nodes("table") %>%
